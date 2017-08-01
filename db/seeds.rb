855a4874c9c8ab456ab222aefcda4ba5c3c2954a
Nathaneil Ward <nathaneilw@gmail.com>
2017-08-01 17:07:19 +0200

    created everything basically

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Article.destroy_all
+10.times do
+  rand_article = Article.new(
+    title: Faker::Hipster.sentence,
+    content: Faker::Hipster.paragraph,
+  )
+  rand_article.save!
+end
+
